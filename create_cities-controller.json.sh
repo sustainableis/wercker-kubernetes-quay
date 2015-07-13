27ed111e10d18cc882906fa101e86d32b8866cdd
John Crawford <jrcrawfo@ncsu.edu>
2015-07-13 16:16:17 -0400

    Update image location

--- create_cities-controller.json.sh
+++ create_cities-controller.json.sh
@@ -26,7 +26,7 @@ cat > cities-controller.json <<EOF
         "containers": [
           {
             "imagePullPolicy": "Always",
-            "image": "quay.io/wercker/wercker-kubernetes-quay:${WERCKER_GIT_COMMIT}",
+            "image": "quay.io/ndustrialio/wercker-test:${WERCKER_GIT_COMMIT}",
             "name": "cities",
             "ports": [
               {
